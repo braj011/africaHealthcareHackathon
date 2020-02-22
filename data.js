3bab759d0367c175b78a1b90c4e0c0ba522dc5f4
mmniazi <mohsinniazikk@gmail.com>
2020-02-22 21:25:49 +0000

    add data

---
+++ data.js
@@ -0,0 +1,152 @@
+eqfeed_callback({
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [-0.117434, 51.517373]},
+            "id": "London",
+            "properties": {
+                "data": {
+                    "January-2020": "9467",
+                    "February-2020": "17018",
+                    "March-2020": "18234",
+                    "April-2020": "15038",
+                    "May-2020": "14611",
+                    "June-2020": "16185",
+                    "July-2020": "14714",
+                    "August-2020": "10811",
+                    "September-2020": "8564",
+                    "October-2020": "8672",
+                    "November-2020": "8670",
+                    "December-2020": "10044",
+                }
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [-1.7936651, 52.9454354]},
+            "id": "Midlands",
+            "properties": {
+                "data": {
+                    "January-2020": "12928",
+                    "February-2020": "10064",
+                    "March-2020": "12017",
+                    "April-2020": "12094",
+                    "May-2020": "11940",
+                    "June-2020": "11500",
+                    "July-2020": "11811",
+                    "August-2020": "12102",
+                    "September-2020": "11910",
+                    "October-2020": "14403",
+                    "November-2020": "15817",
+                    "December-2020": "19395",
+                }
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [1, 52.3]},
+            "id": "East of England",
+            "properties": {
+                "data": {
+                    "January-2020": "7347",
+                    "February-2020": "7593",
+                    "March-2020": "7654",
+                    "April-2020": "7948",
+                    "May-2020": "7377",
+                    "June-2020": "7288",
+                    "July-2020": "8479",
+                    "August-2020": "7912",
+                    "September-2020": "7623",
+                    "October-2020": "9510",
+                    "November-2020": "9692",
+                    "December-2020": "9844",
+                }
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [-0.064706, 51.158354]},
+            "id": "South East",
+            "properties": {
+                "data": {
+                    "January-2020": "7667",
+                    "February-2020": "6583",
+                    "March-2020": "6886",
+                    "April-2020": "6297",
+                    "May-2020": "6733",
+                    "June-2020": "6301",
+                    "July-2020": "7449",
+                    "August-2020": "6737",
+                    "September-2020": "6579",
+                    "October-2020": "7282",
+                    "November-2020": "7737",
+                    "December-2020": "10620",
+                }
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [-1.017573, 53.899567]},
+            "id": "North East",
+            "properties": {
+                "data": {
+                    "January-2020": "8310",
+                    "February-2020": "5878",
+                    "March-2020": "6212",
+                    "April-2020": "6405",
+                    "May-2020": "6358",
+                    "June-2020": "6851",
+                    "July-2020": "8388",
+                    "August-2020": "9087",
+                    "September-2020": "8084",
+                    "October-2020": "10957",
+                    "November-2020": "10812",
+                    "December-2020": "12012",
+                }
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [-2.36368, 54.2557]},
+            "id": "North West",
+            "properties": {
+                "data": {
+                    "January-2020": "8310",
+                    "February-2020": "5878",
+                    "March-2020": "6212",
+                    "April-2020": "6405",
+                    "May-2020": "6358",
+                    "June-2020": "6851",
+                    "July-2020": "8388",
+                    "August-2020": "9087",
+                    "September-2020": "8084",
+                    "October-2020": "10957",
+                    "November-2020": "10812",
+                    "December-2020": "12012",
+                }
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [-2.572638, 51.107475]},
+            "id": "South West",
+            "properties": {
+                "data": {
+                    "January-2020": "5249",
+                    "February-2020": "3079",
+                    "March-2020": "3452",
+                    "April-2020": "3445",
+                    "May-2020": "3000",
+                    "June-2020": "3396",
+                    "July-2020": "4005",
+                    "August-2020": "4283",
+                    "September-2020": "4576",
+                    "October-2020": "5189",
+                    "November-2020": "5130",
+                    "December-2020": "7305",
+                }
+            }
+        }
+    ]
+});
